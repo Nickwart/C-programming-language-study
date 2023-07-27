a7d3ec476cafcfba644d97bea814c3aeea1ad834
connstanta <hrumeleon@gmail.com>
2023-07-27 13:24:52 +0300

    day 4 start

--- chapter-01/character-input-output.c
+++ chapter-01/character-input-output.c
@@ -20,12 +20,75 @@ copy input to output */
 /* Version 2
 assigment in while loop*/
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int c;
-    while((c = getchar()) != EOF) {
-        putchar(c);
-    }
-}
+// int main() {
+//     int c;
+//     while((c = getchar()) != EOF) {
+//         printf("%d\n", (getchar() != EOF));
+//         putchar(c);
+//     }
+// }
 
+
+
+
+
+/* Version 3, exercise 1.7(write a program to print EOF value)*/
+
+// #include <stdio.h>
+
+// int main() {
+//     printf("-------------------\n");
+//     printf("%d\n", EOF);
+//     printf("-------------------\n");
+// }
+
+
+/*---------------------------------------------------------------------------------*/
+
+
+/* Version 1, character counting*/
+
+// #include <stdio.h>
+
+// #include <stdbool.h>
+
+
+// int main() {
+//     long nc;
+//     nc = 0;
+//     while(getchar() != EOF) {
+//         ++nc;
+//         printf("%ld\n", nc);
+//     }
+// }
+
+// int main() {
+//     long nc;
+//     nc = 0;
+//     while(true) {
+//         ++nc;
+//         printf("%ld\n", nc);
+//     }
+// }
+
+
+
+
+
+/* Vesion 2, Character count */
+
+// #include <stdio.h>
+
+// int main() {
+//     double nc;
+
+//     for(nc = 0; getchar() != EOF; ++nc) {
+//         printf("%.0f\n", nc);
+//     }
+// }
+
+
+
+/* Version 3, Line counting */
